8d890c3b9109b59608bf685a531189d10db1d833
ss <setrotec@outlook.com>
2021-09-03 17:24:20 -0300

    Trabalhando com datas

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -15,6 +15,13 @@ class NegociacaoController {
 
     adiciona(event){
         event.preventDefault();
+
+        /*O método replace() retorna uma nova string com algumas
+        ou todas as correspondências de um padrão substituídas por
+        um determinado caractere (ou caracteres).
+        O padrão pode ser uma string ou uma RegExp, e a substituição
+        pode ser uma string ou uma função a ser chamada para cada correspondência.
+        Se o padrão for uma string, apenas a primeira ocorrência será substituída.*/
         let data = new Date(this._inputData.value.replace(/-/g, ','));
         console.log(data);
 
